c72c1da0ec5bffe0ef6c4a6e1ae50ee71081a1fa
Rafael Andrioli Bauer <abauer.rafael@gmail.com>
2022-12-18 19:00:34 +0100

    Created files for exerice7

---
+++ exercise7/exercise7.cpp
@@ -0,0 +1,42 @@
+/*****************************************************************************
+* @file                    exercise7.cpp
+* @author                  Rafael Andrioli Bauer
+* @date                    18.12.2022
+* @matriculation number    5163344
+* @e-mail contact          abauer.rafael@gmail.com
+*
+* @brief   Exercise 7 - Watchdog Timers & Timer Modules
+*
+* Description:
+*
+* Pin connections:
+*
+* Theory answers: None
+*
+* Tasks completed:
+*  Task 1
+*    a)
+*         [x] Modify exercise5                       (x/6,0 pt.)
+*    b)
+*         [x] Two advantages compared to exercise 5  (x/1,0 pt.)
+*         [x] Describe signal flow                   (x/2,0 pt.)
+*  Task 2
+*         [x] feedback.txt                           (x/1.0 pt.)
+*
+* @note    The project was exported using CCS 12.1.0.00007
+******************************************************************************/
+#include <templateEMP.h>
+
+
+using namespace Microtech;
+
+
+int main() {
+ initMSP();
+
+ while (true) {
+   _no_operation();
+ }
+ return 0;
+}
+
