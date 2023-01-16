cd86184868129ea03908687294bf07eaa42dad46
rafaelBauer <abauer.rafael@gmail.com>
2023-01-16 17:08:29 +0100

    Finished implementation of signal generator and oscilloscope

--- common/SignalGenerator.hpp
+++ common/SignalGenerator.hpp
@@ -1,12 +1,12 @@
 #ifndef MICROTECH_SIGNALGENERATOR_HPP
 #define MICROTECH_SIGNALGENERATOR_HPP
 
-#include "IQmathLib.h"
 #include <cstdint>
+#include "IQmathLib.h"
 
 namespace Microtech {
 
-#define PI      3.1415926536
+#define PI 3.1415926536
 
 /**
  * @class SignalProperties
@@ -26,18 +26,18 @@ public:
    * This constructor also set the initial frequency of the signal to 1 Hz.
    */
   explicit SignalProperties(uint16_t fsHz) : samplingFreqHz(fsHz) {
-    setNewFrequency(1);
+    setNewFrequency(INITIAL_FREQUENCY);
   }
 
   /**
    * @brief set the new frequency of the signal
    * @param[in] newFrequency new frequency of the signal
    */
-  void setNewFrequency(const uint16_t newFrequency) {
-    currentFrequency = _IQ15(newFrequency);
+  void setNewFrequency(const _iq15 newFrequency) {
+    currentFrequency = newFrequency;
     // 2*pi*currentFrequency/samplingFreqHz
-    const _iq15 test = _IQ15mpy(_IQ15(2*PI),currentFrequency);
-    phaseStep = _IQ15div(test, _IQ15(samplingFreqHz));
+    const _iq15 freqInCyclesPerSecond = _IQ15mpy(_IQ15(2 * PI), currentFrequency);
+    phaseStep = _IQ15div(freqInCyclesPerSecond, _IQ15(samplingFreqHz));
   }
 
   /**
@@ -45,7 +45,7 @@ public:
    */
   void increasePhase() noexcept {
     // calculate new phase and keep it within 0 and 2pi.
-    currentPhase = (currentPhase + phaseStep)%_IQ15(2*PI);
+    currentPhase = (currentPhase + phaseStep) % _IQ15(2 * PI);
   }
 
   /**
@@ -56,17 +56,17 @@ public:
     return currentPhase;
   }
 
-  uint16_t getCurrentFrequency() const noexcept {
-      return _IQ15int(currentFrequency);
+  _iq15 getCurrentFrequency() const noexcept {
+    return currentFrequency;
   }
 
 private:
-  const uint16_t samplingFreqHz; ///< Sampling frequency of the signal
-  _iq15 currentFrequency = 0; ///< Current frequency of the signal
-  PhaseType phaseStep = 0;  ///< Current phase step of the signal
-  PhaseType currentPhase = 0; ///< Current phase of the signal
+  const uint16_t samplingFreqHz;  ///< Sampling frequency of the signal
+  _iq15 currentFrequency = 0;     ///< Current frequency of the signal
+  PhaseType phaseStep = 0;        ///< Current phase step of the signal
+  PhaseType currentPhase = 0;     ///< Current phase of the signal
+  static constexpr _iq15 INITIAL_FREQUENCY = _IQ15(1.0);
 };
-
 
 /**
  * @class iSignalType
@@ -90,6 +90,9 @@ public:
    * @return next point of the signal
    */
   virtual _iq15 getNextPoint(SignalProperties& signal) noexcept = 0;
+
+protected:
+  static constexpr _iq15 MAXIMUM_AMPLITUDE = _IQ15(100.0);
 };
 
 /**
@@ -108,20 +111,21 @@ public:
    * @return next point of the Sinusoidal signal
    */
   _iq15 getNextPoint(SignalProperties& signal) noexcept override {
-    // value is between 0 and 1
-    // sin gives value from -1 to 1, so we add 1 to get the output from 0 to 2
-    const _iq15 value = _IQ15sin(signal.getCurrentPhase()) + _IQ15(1);
+    2
+      // sin gives value from -1 to 1, so we add 1 to get the output from 0 to 2
+      const _iq15 sineValue = _IQ15sin(signal.getCurrentPhase()) + _IQ15(1);
+    constexpr _iq15 HALF_OF_MAX_AMPLITUDE = _IQ15div(MAXIMUM_AMPLITUDE, _IQ15(2.0));
 
     // Multiply by 50.0 since our maximum value is 100.0 and the maximum from the sine is 2.
-    const _iq15 multiplication = _IQ15mpy(value, _IQ15(50.0));
-    return multiplication;
+    const _iq15 sineScaled = _IQ15mpy(sineValue, HALF_OF_MAX_AMPLITUDE);
+    return sineScaled;
   }
 };
 
 /**
  * @brief Class representing Trapezoidal signal
  */
-class  Trapezoidal : public iSignalType {
+class Trapezoidal : public iSignalType {
 public:
   /**
    * @brief default constructor
@@ -133,41 +137,90 @@ public:
    * @param[in] signal Signal properties
    * @return next point of the Trapezoidal signal
    */
-  _iq15 getNextPoint(SignalProperties& signal) noexcept override{
+  _iq15 getNextPoint(SignalProperties& signal) noexcept override {
     const _iq15 currentPhase = signal.getCurrentPhase();
-    constexpr _iq15 DEG30_IN_RAD = _IQ15(PI/6);  // 30 degrees in radians
-    constexpr _iq15 DEG60_IN_RAD = _IQ15(PI/3);  // 60 degrees in radians
-    constexpr _iq15 DEG120_IN_RAD = _IQ15(2*PI/3); // 120 degrees in radians
-    constexpr _iq15 DEG150_IN_RAD = DEG30_IN_RAD + DEG120_IN_RAD; // 150 degrees in radians
-    constexpr _iq15 DEG210_IN_RAD = DEG150_IN_RAD + DEG60_IN_RAD; // 210 degrees in radians
-    constexpr _iq15 DEG330_IN_RAD = DEG210_IN_RAD + DEG120_IN_RAD; // 330 degrees in radians
+    const _iq15 slope = getSlope();
+    constexpr _iq15 yIntercept = getYIntercept();
 
-    const _iq15 slope = _IQ15div(_IQ15(100.0),DEG60_IN_RAD);
-
-    _iq15 retVal = 0;
-    // check for the current phase
-
-    if (currentPhase < DEG30_IN_RAD) {
-      retVal = _IQ15mpy(currentPhase,slope) + _IQ15(50.0);
-    } else if(currentPhase > DEG30_IN_RAD && currentPhase < DEG150_IN_RAD) {
-      retVal = _IQ15(100.0);
-    } else if(currentPhase > DEG150_IN_RAD && currentPhase < DEG210_IN_RAD) {
-      //retVal = -currentPhase*slope + 3500;
-      retVal = _IQ15mpy(_IQ15(-1),_IQ15mpy(currentPhase,slope)) + _IQ15(350.0);
-    } else if(currentPhase > DEG210_IN_RAD && currentPhase < DEG330_IN_RAD) {
-      retVal = _IQ15(0);
-    } else if(currentPhase > DEG330_IN_RAD) {
-      //retVal = currentPhase*slope - 5500;
-      retVal = _IQ15mpy(currentPhase,slope) - _IQ15(550.0);
+    // Use a switch case to check for the current phase
+    switch (getCurrentPhase(currentPhase)) {
+      case PHASE_1: return slope * currentPhase + yIntercept;
+      case PHASE_2: return MAXIMUM_AMPLITUDE;
+      case PHASE_3:
+        // The last constant value has to be added to compensate since the angle is higher
+        return slope * currentPhase + yIntercept + _IQ(300.0);
+      case PHASE_4: return _IQ15(0.0);
+      case PHASE_5: return slope * currentPhase + yIntercept + _IQ(500.0);
+      default: return _IQ15(0.0);
     }
-    return retVal;
   }
+
+private:
+  /**
+   * @brief Get the slope of the Trapezoidal signal
+   * @return The slope of the Trapezoidal signal
+   */
+  _iq15 getSlope() const noexcept {
+    return _IQ15div(MAXIMUM_AMPLITUDE, DEG60_IN_RAD);
+  }
+
+  /**
+   * @brief Get the y-intercept of the Trapezoidal signal
+   * @return The y-intercept of the Trapezoidal signal
+   */
+  constexpr _iq15 getYIntercept() const noexcept {
+    return _IQ15(50.0);
+  }
+
+  // define a struct to store the phase intervals
+  struct PhaseInterval {
+    _iq15 start;
+    _iq15 end;
+  };
+
+  const PhaseInterval PHASE_1 = {DEG0_IN_RAD, DEG30_IN_RAD};
+  const PhaseInterval PHASE_2 = {DEG30_IN_RAD, DEG150_IN_RAD};
+  const PhaseInterval PHASE_3 = {DEG150_IN_RAD, DEG210_IN_RAD};
+  const PhaseInterval PHASE_4 = {DEG210_IN_RAD, DEG330_IN_RAD};
+  const PhaseInterval PHASE_5 = {DEG330_IN_RAD, DEG360_IN_RAD};
+
+  /**
+   * @brief Get the current phase of the signal
+   * @param[in] currentPhase The current phase of the signal
+   * @return The current phase of the signal
+   */
+  PhaseInterval getCurrentPhase(const _iq15 currentPhase) const {
+    if (currentPhase >= PHASE_1.start && currentPhase < PHASE_1.end) {
+      return PHASE_1;
+    } else if (currentPhase >= PHASE_2.start && currentPhase < PHASE_2.end) {
+      return PHASE_2;
+    } else if (currentPhase >= PHASE_3.start && currentPhase < PHASE_3.end) {
+      return PHASE_3;
+    } else if (currentPhase >= PHASE_4.start && currentPhase < PHASE_4.end) {
+      return PHASE_4;
+    } else if (currentPhase >= PHASE_5.start && currentPhase < PHASE_5.end) {
+      return PHASE_5;
+    } else {
+      return PHASE_1;
+    }
+  }
+
+  static constexpr _iq15 DEG0_IN_RAD = _IQ15(0);
+  static constexpr _iq15 DEG30_IN_RAD = _IQ15(PI / 6);
+  static constexpr _iq15 DEG60_IN_RAD = _IQ15(PI / 3);
+  static constexpr _iq15 DEG120_IN_RAD = _IQ15(2 * PI / 3);
+  static constexpr _iq15 DEG150_IN_RAD = DEG30_IN_RAD + DEG120_IN_RAD;
+  static constexpr _iq15 DEG210_IN_RAD = DEG150_IN_RAD + DEG60_IN_RAD;
+  static constexpr _iq15 DEG330_IN_RAD = DEG210_IN_RAD + DEG120_IN_RAD;
+  static constexpr _iq15 DEG360_IN_RAD = _IQ15(2 * PI);
 };
+
+// That was my implementation, can you come up with a better implementation for getNextPoint
 
 /**
  * @brief Class representing Rectangular signal
  */
-class  Rectangular : public iSignalType {
+class Rectangular : public iSignalType {
 public:
   /**
    * @brief default constructor
@@ -179,10 +232,10 @@ public:
    * @param[in] signal Signal properties
    * @return next point of the Rectangular signal
    */
-  _iq15 getNextPoint(SignalProperties& signal) noexcept override{
+  _iq15 getNextPoint(SignalProperties& signal) noexcept override {
     _iq15 retVal = 0;
     if (signal.getCurrentPhase() < _IQ15(PI)) {
-      retVal = _IQ15(100.0);
+      retVal = MAXIMUM_AMPLITUDE;
     } else {
       retVal = _IQ15(0);
     }
@@ -200,9 +253,9 @@ public:
    * @brief Enumeration of different signal shapes that can be generated.
    */
   enum class Shape {
-    SINUSOIDAL, ///< Signal shape representing sinusoidal signal
-    TRAPEZOIDAL, ///< Signal shape representing trapezoidal signal
-    RECTANGULAR, ///< Signal shape representing rectangular signal
+    SINUSOIDAL,   ///< Signal shape representing sinusoidal signal
+    TRAPEZOIDAL,  ///< Signal shape representing trapezoidal signal
+    RECTANGULAR,  ///< Signal shape representing rectangular signal
   };
 
   /**
@@ -216,16 +269,17 @@ public:
    * @brief get the next data point of the active signal
    * @return next data point
    */
-  _iq15 getNextDatapoint() noexcept{
+  _iq15 getNextDatapoint() noexcept {
     signalProperties.increasePhase();
-    return _IQ15mpy(activeSignal->getNextPoint(signalProperties), outputAmplitudePercentage) + _IQ15mpy(_IQ15(1.0) - outputAmplitudePercentage, _IQ15(100.0));
+    return _IQ15mpy(activeSignal->getNextPoint(signalProperties), outputAmplitudePercentage)
+           + _IQ15mpy(_IQ15(1.0) - outputAmplitudePercentage, _IQ15(100.0));
   }
 
   /**
    * @brief set a new frequency for the signal
    * @param[in] newFrequency new frequency
    */
-  void setNewFrequency(const uint16_t newFrequency) noexcept{
+  void setNewFrequency(const _iq15 newFrequency) noexcept {
     signalProperties.setNewFrequency(newFrequency);
   }
 
@@ -235,66 +289,91 @@ public:
    */
   void setActiveSignalShape(const Shape newShape) noexcept {
     switch (newShape) {
-      case Shape::SINUSOIDAL:
-        activeSignal = &sinusoidal;
-        break;
-      case Shape::TRAPEZOIDAL:
-        activeSignal = &trapezoidal;
-        break;
-      case Shape::RECTANGULAR:
-        activeSignal = &rectangular;
-        break;
+      case Shape::SINUSOIDAL: activeSignal = &sinusoidal; break;
+      case Shape::TRAPEZOIDAL: activeSignal = &trapezoidal; break;
+      case Shape::RECTANGULAR: activeSignal = &rectangular; break;
     }
   }
 
+  /**
+   * @brief This function changes the active signal shape to the next shape in the list of available shapes
+   */
   void nextSignalShape() {
-      if(shapeIndex < 2) {
-          shapeIndex++;
-          setActiveSignalShape((Shape)shapeIndex);
-      }
+    if (shapeIndex < 2) {
+      shapeIndex++;
+      setActiveSignalShape((Shape)shapeIndex);
+    }
   }
 
+  /**
+   * @brief This function changes the active signal shape to the previous shape in the list of available shapes
+   */
   void previousSignalShape() {
-    if(shapeIndex > 0) {
-        shapeIndex--;
-        setActiveSignalShape((Shape)shapeIndex);
+    if (shapeIndex > 0) {
+      shapeIndex--;
+      setActiveSignalShape((Shape)shapeIndex);
     }
   }
 
+  /**
+   * @brief This function increases the frequency of the signal by a fixed step value
+   * of SignalGenerator::FREQUENCY_STEP:
+   */
   void increaseFrequency() {
-      uint16_t currentFrequency = signalProperties.getCurrentFrequency();
-      if(currentFrequency < 5) {
-          signalProperties.setNewFrequency(currentFrequency + 1);
-      }
-  }
-
-  void decreaseFrequency() {
-      uint16_t currentFrequency = signalProperties.getCurrentFrequency();
-      if(currentFrequency > 1) {
-          signalProperties.setNewFrequency(currentFrequency - 1);
-      }
-  }
-
-  void increaseAmplitude() {
-      if(outputAmplitudePercentage < _IQ15(1.0)) {
-          outputAmplitudePercentage += _IQ15(0.05);
-      }
-  }
-  void decreaseAmplitude() {
-        if(outputAmplitudePercentage > _IQ15(0.0)) {
-            outputAmplitudePercentage -= _IQ15(0.05);
-        }
+    const _iq15 currentFrequency = signalProperties.getCurrentFrequency();
+    if (currentFrequency < MAXIMUM_FREQUENCY) {
+      signalProperties.setNewFrequency(currentFrequency + FREQUENCY_STEP);
     }
-private:
-  Sinusoidal sinusoidal; ///< Sinusoidal signal object
-  Trapezoidal trapezoidal; ///< Trapezoidal signal object
-  Rectangular rectangular; ///< Rectangular signal object
-  iSignalType* activeSignal; ///< pointer to the active signal
-  SignalProperties signalProperties; ///< Signal Properties object
-  _iq15 outputAmplitudePercentage = _IQ15(1.0);
-  uint8_t shapeIndex = 0;
-};
+  }
+  /**
+   * @brief This function decreases the frequency of the signal by a fixed step value
+   * of SignalGenerator::FREQUENCY_STEP:
+   */
+  void decreaseFrequency() {
+    const _iq15 currentFrequency = signalProperties.getCurrentFrequency();
+    if (currentFrequency > MINIMUM_FREQUENCY) {
+      signalProperties.setNewFrequency(currentFrequency - FREQUENCY_STEP);
+    }
+  }
 
+  /**
+   * @brief increase the amplitude of the active signal by a fixed step
+   */
+  void increaseAmplitude() {
+    if (outputAmplitudePercentage < _IQ15(1.0)) {
+      outputAmplitudePercentage += AMPLITUDE_STEP;
+    }
+  }
+
+  /**
+   * @brief decrease the amplitude of the active signal by a fixed step
+   */
+  void decreaseAmplitude() {
+    if (outputAmplitudePercentage > _IQ15(0.0)) {
+      outputAmplitudePercentage -= AMPLITUDE_STEP;
+    }
+  }
+
+private:
+  Sinusoidal sinusoidal;              ///< Sinusoidal signal object
+  Trapezoidal trapezoidal;            ///< Trapezoidal signal object
+  Rectangular rectangular;            ///< Rectangular signal object
+  iSignalType* activeSignal;          ///< pointer to the active signal
+  SignalProperties signalProperties;  ///< Signal Properties object
+  _iq15 outputAmplitudePercentage =
+    _IQ15(1.0);  ///< Represents the amplitude of the output signal as a percentage. It is initialized to 100%.
+  uint8_t shapeIndex = 0;  ///< Represents the current shape of the active signal. It is initialized to 0 = SINUSOIDAL.
+
+  constexpr _iq15 MAXIMUM_FREQUENCY =
+    _IQ15(5.0);  ///< Represents the maximum frequency that the signal generator can output. It is initialized to 5 Hz.
+  constexpr _iq15 MINIMUM_FREQUENCY = _IQ15(
+    0.5);  ///< Represents the minimum frequency that the signal generator can output. It is initialized to 0.5 Hz.
+  constexpr _iq15 FREQUENCY_STEP =
+    _IQ15(0.5);  ///< Represents the step size for frequency changes. It is initialized to 0.5 Hz.
+
+  constexpr _iq15 AMPLITUDE_STEP =
+    _IQ15(0.05);  ///< Represents the step size for amplitude changes. It is initialized to 0.05 (5%).
+};
 
 }  // namespace Microtech
 
--- exercise8/exercise8.cpp
+++ exercise8/exercise8.cpp
@@ -1,38 +1,42 @@
 /*****************************************************************************
-* @file                    exercise8.cpp
-* @author                  Rafael Andrioli Bauer
-* @date                    11.01.2023
-* @matriculation number    5163344
-* @e-mail contact          abauer.rafael@gmail.com
-*
-* @brief   Exercise 9 - Project Option 1 - Oscilloscope and Signal generator
-*
-* Description:
-*
-* Pin connections:
-*       CON3:P1.0 <-> DAC_OUT
-*       JP5 -> INT
-*       CON3:P3.6 <-> DAC_IN
-*       CON3:P1.5 <-> PB5
-*       CON3:P1.6 <-> PB6
-*
-* Theory answers: None
-*
-* Tasks completed:
-*
-*
-* @note    The project was exported using CCS 12.1.0.00007
-******************************************************************************/
+ * @file                    exercise8.cpp
+ * @author                  Rafael Andrioli Bauer
+ * @date                    11.01.2023
+ * @matriculation number    5163344
+ * @e-mail contact          abauer.rafael@gmail.com
+ *
+ * @brief   Exercise 9 - Project Option 1 - Oscilloscope and Signal generator
+ *
+ * Description:     The software includes the functionality to generate sinusoidal,
+ *                  trapezoidal, and rectangular signals with a maximum frequency of 5Hz,
+ *                  a minimum frequency of 0.5Hz, and a frequency step of 0.5Hz.
+ *                  The amplitude of the signals can also be adjusted with an amplitude step of 0.05.
+ *
+ *                  The oscilloscope prints new values every 20ms.
+ *
+ * Pin connections:
+ *       CON3:P1.0 <-> DAC_OUT
+ *       JP5 -> INT
+ *       CON3:P3.6 <-> DAC_IN
+ *       CON3:P1.5 <-> PB5
+ *       CON3:P1.6 <-> PB6
+ *
+ * Theory answers: None
+ *
+ * Tasks completed:
+ *
+ *
+ * @note    The project was exported using CCS 12.1.0.00007
+ ******************************************************************************/
 #include <templateEMP.h>
 
+#include "Adc.hpp"
 #include "Button.hpp"
 #include "GPIOs.hpp"
-#include "Adc.hpp"
-#include "ShiftRegister.hpp"
-#include "Timer.hpp"
 #include "Pwm.hpp"
+#include "ShiftRegister.hpp"
 #include "SignalGenerator.hpp"
-
+#include "Timer.hpp"
 
 using namespace Microtech;
 
@@ -43,146 +47,137 @@ Button btnIncreaseAmplitude(GPIOs::getInputHandle<IOPort::PORT_1, static_cast<ui
 // Oscilloscope channel
 AdcHandle adcCH1 = Adc::getInstance().getAdcHandle<0>();
 
+// Signal Generator with sampling frequency of 50Hz = every 20ms.
 SignalGenerator signalGenerator(50);
-Pwm DAC_IN(GPIOs::getOutputHandle<IOPort::PORT_3, static_cast<uint8_t>(6)>());  // Create handle of PWM for pin 6 from port 3
+// Create handle of PWM for pin 6 from port 3
+Pwm DAC_IN(GPIOs::getOutputHandle<IOPort::PORT_3, static_cast<uint8_t>(6)>());
 
 constexpr ShiftRegisterPB pb1to4(GPIOs::getOutputHandle<IOPort::PORT_2, static_cast<uint8_t>(4)>(),
-                       GPIOs::getOutputHandle<IOPort::PORT_2, static_cast<uint8_t>(5)>(),
-                       GPIOs::getOutputHandle<IOPort::PORT_2, static_cast<uint8_t>(2)>(),
-                       GPIOs::getOutputHandle<IOPort::PORT_2, static_cast<uint8_t>(3)>(),
-                       GPIOs::getInputHandle<IOPort::PORT_2, static_cast<uint8_t>(7)>());
+                                 GPIOs::getOutputHandle<IOPort::PORT_2, static_cast<uint8_t>(5)>(),
+                                 GPIOs::getOutputHandle<IOPort::PORT_2, static_cast<uint8_t>(2)>(),
+                                 GPIOs::getOutputHandle<IOPort::PORT_2, static_cast<uint8_t>(3)>(),
+                                 GPIOs::getInputHandle<IOPort::PORT_2, static_cast<uint8_t>(7)>());
 
-uint16_t adcValue = 0;
-uint16_t setpoint = 0;
-bool newValue = false;
-
+/**
+ * @brief Decreases the value of the given counter.
+ * If the counter is already at 0, it will stay at 0.
+ * @param[in, out] counter The counter that will be decreased.
+ */
 void decreaseCounter(uint8_t &counter) {
-    if(counter > 0) {
-        counter--;
-    }
+  if (counter > 0) {
+    counter--;
+  }
 }
 
+/**
+ * @brief Interrupt service routine called by the timer
+ * It will read the values of the oscilloscope and send it to the serial port
+ * It also handles the debouncing of the buttons and updates the signal generator's
+ * amplitude and frequency based on user input.
+ */
 void timerInterrupt() {
-    _iq15 nextDatapoint = signalGenerator.getNextDatapoint();
-    // Print values of Oscilloscope
-    adcValue = adcCH1.getRawValue();
-    setpoint = _IQ15int(nextDatapoint);
-    newValue = true;
-    serialPrintInt(adcCH1.getRawValue());
-   // serialPrint(" ");
-    //serialPrintInt(_IQ15int(nextDatapoint));
-    serialPrintln("");
+  _iq15 nextDatapoint = signalGenerator.getNextDatapoint();
+  // Print values of Oscilloscope
+  serialPrintInt(adcCH1.getRawValue());
+  serialPrintln("");
 
-    uint8_t PBvalues = pb1to4.getPBValues();
-    static uint8_t PB1debounceCnt = 0;
-    static uint8_t PB2debounceCnt = 0;
-    static uint8_t PB3debounceCnt = 0;
-    static uint8_t PB4debounceCnt = 0;
+  // Get the current PB values of the shift register (PB1-4)
+  uint8_t PBvalues = pb1to4.getPBValues();
 
-    if(PBvalues & (0x01) && !PB1debounceCnt) { //PB1
-        signalGenerator.nextSignalShape();
-        PB1debounceCnt = 5;
-    } else if(PBvalues & (0x01 << 1) && !PB2debounceCnt) {  // PB2
-        signalGenerator.previousSignalShape();
-        PB2debounceCnt = 5;
-    }
+  static uint8_t PB1debounceCnt = 0;
+  static uint8_t PB2debounceCnt = 0;
+  static uint8_t PB3debounceCnt = 0;
+  static uint8_t PB4debounceCnt = 0;
 
-    decreaseCounter(PB1debounceCnt);
-    decreaseCounter(PB2debounceCnt);
+  // Check if buttons have been pressed and perform debounce
+  if (PBvalues & (0x01) && !PB1debounceCnt) {  // PB1
+    signalGenerator.nextSignalShape();
+    PB1debounceCnt = 5;                                    // Start debouncer counter
+  } else if (PBvalues & (0x01 << 1) && !PB2debounceCnt) {  // PB2
+    signalGenerator.previousSignalShape();
+    PB2debounceCnt = 5;
+  }
 
-    if(PBvalues & (0x01 << 2) && !PB3debounceCnt) { //PB3
-        signalGenerator.increaseFrequency();
-        PB3debounceCnt = 5;
-    } else if(PBvalues & (0x01 << 3) && !PB4debounceCnt) {  // PB4
-        signalGenerator.decreaseFrequency();
-        PB4debounceCnt = 5;
-    }
-    decreaseCounter(PB3debounceCnt);
-    decreaseCounter(PB4debounceCnt);
+  if (PBvalues & (0x01 << 2) && !PB3debounceCnt) {  // PB3
+    signalGenerator.increaseFrequency();
+    PB3debounceCnt = 5;
+  } else if (PBvalues & (0x01 << 3) && !PB4debounceCnt) {  // PB4
+    signalGenerator.decreaseFrequency();
+    PB4debounceCnt = 5;
+  }
 
-    btnDecreaseAmplitude.evaluateDebounce();
-    btnIncreaseAmplitude.evaluateDebounce();
+  decreaseCounter(PB1debounceCnt);
+  decreaseCounter(PB2debounceCnt);
+  decreaseCounter(PB3debounceCnt);
+  decreaseCounter(PB4debounceCnt);
+  btnDecreaseAmplitude.evaluateDebounce();
+  btnIncreaseAmplitude.evaluateDebounce();
 
-    DAC_IN.setDutyCycle(nextDatapoint);
+  // Update PWM output duty-cycle
+  DAC_IN.setDutyCycle(nextDatapoint);
 }
 
+/**
+ * @brief callback function that is called when the decrease amplitude button (PB5) is pressed
+ * It calls the decreaseAmplitude() function of the signalGenerator
+ * @param buttonState state of the button (not used in this case)
+ */
 void decreaseAmplitudeCallback(ButtonState /*buttonState*/) {
-    signalGenerator.decreaseAmplitude();
+  signalGenerator.decreaseAmplitude();
 }
 
+/**
+ * @brief callback function that is called when the increase amplitude button (PB6) is pressed
+ * It calls the increaseAmplitude() function of the signalGenerator
+ * @param buttonState state of the button (not used in this case)
+ */
 void increaseAmplitudeCallback(ButtonState /*buttonState*/) {
-    signalGenerator.increaseAmplitude();
-
+  signalGenerator.increaseAmplitude();
 }
 
 int main() {
- initMSP();
+  initMSP();
 
- btnDecreaseAmplitude.init();
- btnDecreaseAmplitude.registerPressedStateChangeCallback(&decreaseAmplitudeCallback);
+  btnDecreaseAmplitude.init();
+  btnDecreaseAmplitude.registerPressedStateChangeCallback(&decreaseAmplitudeCallback);
 
- btnIncreaseAmplitude.init();
- btnIncreaseAmplitude.registerPressedStateChangeCallback(&increaseAmplitudeCallback);
+  btnIncreaseAmplitude.init();
+  btnIncreaseAmplitude.registerPressedStateChangeCallback(&increaseAmplitudeCallback);
 
- pb1to4.init();
+  pb1to4.init();
 
- DAC_IN.init();
- DAC_IN.setPwmPeriod<250, std::chrono::microseconds>(); //4kHz
+  DAC_IN.init();
+  DAC_IN.setPwmPeriod<250, std::chrono::microseconds>();  // 4kHz
 
+  Adc::getInstance().init();
+  Adc::getInstance().startConversion();
 
- Adc::getInstance().init();
- Adc::getInstance().startConversion();
+  // Timer with CLK_DIV = 8 and since the period of SMCLK is 1us we also let the timer know that.
+  constexpr TimerConfigBase<8, 1> TIMER_CONFIG(TimerClockSource::Option::SMCLK);
+  Timer<1>::getTimer().init(TIMER_CONFIG);
+  // Creates a 20ms periodic task
+  TaskHandler<20, std::chrono::milliseconds> timerTask(&timerInterrupt, true);
+  Timer<1>::getTimer().registerTask(TIMER_CONFIG, timerTask);
 
- // Timer with CLK_DIV = 8 and since the period of SMCLK is 1us we also let the timer know that.
- constexpr TimerConfigBase<8, 1> TIMER_CONFIG(TimerClockSource::Option::SMCLK);
- Timer<1>::getTimer().init(TIMER_CONFIG);
- // Creates a 10ms periodic task
- TaskHandler<20, std::chrono::milliseconds> timerTask(&timerInterrupt, true);
- Timer<1>::getTimer().registerTask(TIMER_CONFIG, timerTask);
+  while (true) {
+    _no_operation();
+  }
 
- //signalGenerator.setActiveSignalShape(SignalGenerator::Shape::TRAPEZOIDAL);
- while (true) {
-     /*
-     uint8_t PBvalues = pb1to4.getPBValues();
-
-     if(PBvalues & (0x01)) { //PB1
-         signalGenerator.nextSignalShape();
-     } else if(PBvalues & (0x01 << 1)) {  // PB2
-         signalGenerator.previousSignalShape();
-     }
-
-     if(PBvalues & (0x01 << 2)) { //PB3
-         signalGenerator.increaseFrequency();
-     } else if(PBvalues & (0x01 << 3)) {  // PB4
-         signalGenerator.decreaseFrequency();
-     }
-     if(newValue) {
-         serialPrintInt(adcValue);
-         //serialPrint(" ");
-         //serialPrintInt(setpoint);
-         serialPrintln("");
-         newValue = false;
-     }
-     */
- }
-
- return 0;
+  return 0;
 }
 
 // Port 1 interrupt vector
 #pragma vector = PORT1_VECTOR
 __interrupt void Port_1_ISR(void) {
- // Get bit 5 and 6 => 0110 0000 = 0x60
- volatile const uint8_t pendingInterrupt = getRegisterBits(P1IFG, static_cast<uint8_t>(0x60), static_cast<uint8_t>(5));
+  // Get bit 5 and 6 => 0110 0000 = 0x60
+  volatile const uint8_t pendingInterrupt = getRegisterBits(P1IFG, static_cast<uint8_t>(0x60), static_cast<uint8_t>(5));
 
- if(pendingInterrupt > 1) {
-     btnIncreaseAmplitude.getDebouncer().pinStateChanged();
- } else {
-     btnDecreaseAmplitude.getDebouncer().pinStateChanged();
- }
- //
+  if (pendingInterrupt > 1) {
+    btnIncreaseAmplitude.getDebouncer().pinStateChanged();
+  } else {
+    btnDecreaseAmplitude.getDebouncer().pinStateChanged();
+  }
 
- const uint8_t clearFlag = (pendingInterrupt << 0x05);
- resetRegisterBits(P1IFG, clearFlag);  // clear interrupt flag
+  const uint8_t clearFlag = (pendingInterrupt << 0x05);
+  resetRegisterBits(P1IFG, clearFlag);  // clear interrupt flag
 }
-
